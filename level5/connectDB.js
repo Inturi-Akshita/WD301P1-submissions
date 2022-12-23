815c4be26ec6dc5d89ea080296da2a6fd11f5501
Akshita Inturi <88448593+Inturi-Akshita@users.noreply.github.com>
2022-12-23 20:14:44 +0530

    todo database created and connected

--- level5/connectDB.js
+++ level5/connectDB.js
@@ -2,20 +2,11 @@ const Sequelize = require("sequelize");
 
 const database = "todo_db";
 const username = "postgres";
-const password = "@  20WH1A0592";
+const password = "akshita31";
 const sequelize = new Sequelize(database, username, password, {
-  host: "localhost",
+  host: "127.0.0.1",
   dialect: "postgres",
 });
-
-// sequelize
-//   .authenticate()
-//   .then(() => {
-//     console.log("Connection has been established successfully.");
-//   })
-//   .catch((error) => {
-//     console.error("Unable to connect to the database:", error);
-//   });
 
 const connect = async () => {
   return sequelize.authenticate();
