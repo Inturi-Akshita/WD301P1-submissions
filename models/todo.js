09690ccab1373262049aeeffa6e19610e45553b0
Inturi-Akshita <inturiakshita@gmail.com>
2023-01-20 18:53:48 +0530

    created separate DB for testing

--- models/todo.js
+++ models/todo.js
@@ -9,8 +9,17 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
+    // eslint-disable-next-line no-unused-vars
+    static async addTask(params) {
+      return  await Todo.create(params);
+    }
+    // eslint-disable-next-line no-unused-vars
     static associate(models) {
       // define association here
+    }
+    displayableString() {
+      let checkbox = this.completed ? "[x]" : "[ ]";
+      return `${this.id}. ${checkbox} ${this.title} ${this.dueDate}`;
     }
   }
   Todo.init({
