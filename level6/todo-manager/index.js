9ab284c38d0053bb5791ff34a8dc22992e33b871
Inturi-Akshita <inturiakshita@gmail.com>
2023-01-22 15:50:54 +0530

    create and update database

--- level6/todo-manager/index.js
+++ level6/todo-manager/index.js
@@ -1,5 +1,9 @@
+const { request, response } = require("express");
 const express = require("express");
 const app = express();
+const { Todo } = require("./models");
+const bodyParser = require("body-parser"); // to read data from request.body (to look for todo title and duedate)
+app.use(bodyParser.json());
 
 // Parent page
 app.get("/", (request, response) => {
@@ -12,13 +16,41 @@ app.get("/todos", (request, response) => {
 });
 
 // route for creating a new todo
-app.post("/todos", (request, response) => {
+app.post("/todos", async (request, response) => {
   console.log("Creating a todo", request.body);
+  // Todo
+
+  try {
+    // Creating a new method from sequelize
+    const todo = await Todo.addTodo({
+      title: request.body.title,
+      dueDate: request.body.dueDate,
+      completed: false,
+    });
+
+    // to send the todo object back as a response
+    return response.json(todo);
+  } catch (error) {
+    console.log(error);
+    // 422 - unprocessable entity
+    return response.status(422).json(error);
+  }
 });
 
 // To mark as completed
-app.put("/todos/:id/markAsCompleted", (request, response) => {
+app.put("/todos/:id/markAsCompleted", async (request, response) => {
   console.log("We have to update a todo with ID:", request.params.id);
+
+  // update a todo as markAsCompleted
+  const todo = await Todo.findByPk(request.params.id);
+
+  try {
+    const updatedTodo = await todo.markAsCompleted();
+    return response.json(updatedTodo);
+  } catch (error) {
+    console.log(error);
+    return response.status(422).json(error);
+  }
 });
 
 // To delete a todo by its id
