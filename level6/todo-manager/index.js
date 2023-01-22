ff1c0d7c0583a0a10bba06e6701734ac66377a3b
Inturi-Akshita <inturiakshita@gmail.com>
2023-01-22 14:43:27 +0530

    Added routes

--- level6/todo-manager/index.js
+++ level6/todo-manager/index.js
@@ -1,11 +1,30 @@
 const express = require("express");
 const app = express();
 
+// Parent page
 app.get("/", (request, response) => {
   response.send("hello world");
 });
 
+// To print the list of todos
+app.get("/todos", (request, response) => {
+  console.log("Todo list");
+});
 
+// route for creating a new todo
+app.post("/todos", (request, response) => {
+  console.log("Creating a todo", request.body);
+});
+
+// To mark as completed
+app.put("/todos/:id/markAsCompleted", (request, response) => {
+  console.log("We have to update a todo with ID:", request.params.id);
+});
+
+// To delete a todo by its id
+app.delete("/todos/:id", (request, response) => {
+  console.log("Delete a todo by ID: ", request.params.id);
+});
 
 app.listen(3000, () => {
   console.log("Started express server at port 3000");
